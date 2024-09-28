6f613f3e56a7231ec5becad1be278a641e375e14
majogomezg <mariajogogar1@gmail.com>
2024-09-28 18:02:03 -0500

    correcciones servicio a y front

--- backend/app.py
+++ backend/app.py
@@ -117,4 +117,4 @@ def eliminar_tarea(task_id):
     return jsonify({'message': 'Tarea eliminada'}), 200
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
