7569f9e831f144d831cb1eace615206e8024e5bf
majogomezg <mariajogogar1@gmail.com>
2024-09-28 19:43:03 -0500

    creacion de usuario

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     resolve: {
       alias: {
@@ -10,5 +13,11 @@ export default defineConfig({
         '@layouts': '/src/layouts'
       }
     }
-  }
+  },
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
